284b01880291347626afd241e99da04f399f9d8c
spruitje12 <152178387+spruitje12@users.noreply.github.com>
2024-09-30 13:17:10 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -23,3 +23,43 @@ function sluitMenu(){
     deBody.classList.remove("noScroll");
 }
 
+
+let buttonMeer = document.querySelector(".pagina2");
+
+ if (buttonMeer){
+    buttonMeer.onclick = openMeer;
+ }
+
+function openMeer(){
+   let meerLaden = document.querySelector(".meerLaden");
+   if (meerLaden){
+    document.querySelector(".meerLaden").style.display = "grid";
+    buttonMeer.style.display = 'none'
+   }
+}
+
+let R = document.querySelector(':root');
+
+function getR(){
+    let rS = getComputedStyle(R);
+    alert("The value of --font-p is:" + rS.getPropertyValue('--font-p'));
+}
+
+function set(){
+    R.style.setProperty('--font-p', '1.5em');
+}
+
+let biggerFont = document.querySelector("button:nth-of-type(2)");
+biggerFont.onclick = set;
+
+/* Biggerfont code met behulp van chatgpt */
+
+
+function scrollToNextSection() {
+    let nextSection = document.querySelector(".target-section");
+    nextSection.scrollIntoView({ behavior: 'smooth' });
+}
+
+let scrollSnap = document.querySelector(".arrow")
+
+scrollSnap.onclick = scrollToNextSection;
